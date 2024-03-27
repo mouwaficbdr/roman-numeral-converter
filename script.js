76d1e647961a8c4a64b6c99953206fe11c629b6f
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-03-27 17:25:46 +0100

    Last Commit

--- script.js
+++ script.js
@@ -3,32 +3,59 @@ const number = document.getElementById('number');
 const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
+const romanNumeral = [
+  ['M', 1000],
+  ['CM', 900],
+  ['D', 500],
+  ['CD', 400],
+  ['C', 100],
+  ['XC', 90],
+  ['L', 50],
+  ['XL', 40],
+  ['X', 10],
+  ['IX', 9],
+  ['V', 5],
+  ['IV', 4],
+  ['I', 1],
+];
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
 convertBtn.addEventListener('click', () => {
-  convertNumeralToRoman(number.value);
-  console.log('Clicked');
+  output.classList.remove('error');
+  output.textContent = convertToRoman(number.value);
 });
 
 number.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
-    convertNumeralToRoman(number.value);
-    console.log('Clicked');
+    output.classList.remove('error');
+    output.textContent = convertToRoman(number.value);
   }
 });
 
-const convertNumeralToRoman = (input) => {
+const convertToRoman = (input) => {
   const value = Number(input);
   output.style.display = 'block';
-  if (value <= 0) {
-    output.textContent = 'Please enter a number greater than or equal to 1';
-    return;
+  //ERROR CASES
+  if (value < 0) {
+    output.classList.add('error');
+    return 'Please enter a number greater than or equal to 1';
   } else if (input === '') {
-    output.textContent = 'Please enter a valid number';
-    return;
+    output.classList.add('error');
+    return 'Please enter a valid number';
   } else if (value >= 4000) {
-    output.textContent = 'Please enter a number less than or equal to 3999';
+    output.classList.add('error');
+    return 'Please enter a number less than or equal to 3999';
+  } else if (value === 0) {
+    return '';
+  } else {
+    //ROMAN CONVERSION
+    for (let i = 0; i < romanNumeral.length; i++) {
+      if (value >= romanNumeral[i][1]) {
+        return romanNumeral[i][0] + convertToRoman(value - romanNumeral[i][1]);
+      }
+    }
   }
 };
